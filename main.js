9eb034bedd5cddb9e23f713117a066c371d8474a
Ramiro Flores <Serk352@gmail.com>
2020-02-11 23:36:00 -0600

    Update main.js
    Added exercises.

--- main.js
+++ main.js
@@ -1,5 +1,192 @@
 //Built-in Objects exercises.
 
+/*---------- String ----------*/
+/*
+Crear una función que reciba un string como parametro y que al ser llamada haga
+un console.log mostrando el length del string recibido.
 
-// String
-// 
+Ex:
+myFunc('myString');
+*/ 
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Number ----------*/
+/*
+crear una función que reciba un number, que haga lo siguiente:
+* Determinar si el numero recibido es un número entero.
+* En caso de ser entero, hacer un console.log avisando que es un entero. 
+* En caso de no ser entero, convertirlo a entero y mostar el nuevo valor usando console.log.
+
+Ex:
+myFunc(number);
+posibles casos:
+a) el número es un entero :3
+b) el número ahora es un entero con valor de (valor) ;)
+*/ 
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Boolean ----------*/
+/*
+Crear una función que reciba un boolean, que al ser llamada haga un console.log mostrando:
+* El numero de caracteres que representan el valor del boolean recibido.
+
+Ex:
+var flag = true;
+myFunc(flag);
+expected result:  4 caracteres.
+*/
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Object ----------*/
+/*
+crear una función que reciba un object, que al ser llamada haga un console.log mostrando:
+* El numero de propiedades que contiene el objeto.
+* El nombre de cada una de las propiedades del objeto.
+* El valor de cada una las propiedades del objeto.
+
+Ex:
+myFunc(obj);
+*/ 
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Function ----------*/
+/*
+Crear una función que reciba una function, que al ser llamada haga un console log mostrando:
+* El número de argumentos que espera la funcion.
+
+Ex:
+var sum = new Function('a','b','c', 'return a + b + c');
+
+myFunc(sum);
+result: 3.
+*/ 
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Array ----------*/
+/*
+Crear una función que reciba un array de strings, un número (desiredLength) y un string (template), 
+que al ser llamda haga lo siguiente:
+*Checar el numero de propiedades que tiene el array.
+*Si el numero de propiedades es menor a desiredLength por 1: 
+    *Rellenar el espacio faltante dentro del array usando template.  
+    *Hacer un console.log del arreglo con los cambios.
+*En caso de que no se cumpla la condición, mencionar si el array es menor o mayor que desiredLength y por cuanto.
+Ex:
+
+var arr = ['Apple', 'Banana'];
+myFunc(arr, 3, 'apple');
+*/ 
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Date ----------*/
+/*
+Crear una función que al ser llamada haga dos console.log, uno mostrando la fecha actual y otro que muestre cuantos milisegundos
+han pasado desde el 1/enero/1970 hasta ahora.
+
+myFunc();
+*/
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Math ----------*/
+/*
+Crear una función que reciba dos numeros (base y exponente). 
+la función al ser llamada usará el valor absoluto de base y lo elevara a la potencia 
+determinada por exponente, al final mostrar este resultado usando console.log.
+
+Ex:
+
+myFunc(-3, 2) 
+result = 9;
+*/
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- RegExp ----------*/
+/*
+Dada el siguiente patrón (/([A-Z])/), crear una funcion que reciba dos strings (str, template).
+* Determine si str cumple con el patrón.
+* Si cumple, remplazar el las incidencias del patron con el valor de template.
+* Mostar el resultado usando console.log.
+* Si no, mostrar un mensaje de no coincidencia usando console.log
+
+Ex:
+myFunc(str, template)
+*/
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
+
+
+
+/*---------- Error ----------*/
+/*
+Crear una funcion que reciba un numero.
+*Si el número es menor o igual a 10, usar console.log para mostrar un mensaje de valor dentro de los paramatros.
+*Si el número es mayor a 10 arrojar un error con el mensaje, el valor esta fuera de los parametros.
+
+Ex: myFunc(number)
+*/
+
+/*------------------------------ Solución ------------------------------------------- */
+
+
+
+/*----------------------------------------------------------------------------------- */
